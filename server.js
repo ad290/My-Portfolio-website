a7dd0d4fa68cc7228ae3c2d45e19134ed4b14e6c
Aditya Pawar <adityapawar02468@gmail.com>
2025-01-13 01:18:53 +0530

    code added for deployment

--- server.js
+++ server.js
@@ -15,6 +15,25 @@ app.use(express.json());
 //routes
 app.use("/api/v1/portfolio", require("./routes/portfolioRoute"));
 
+
+// --------------------------deployment------------------------------
+
+const __dirname1 = path.resolve();
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname1, "/client/build")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname1, "client", "build", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running..");
+  });
+}
+
+// --------------------------deployment------------------------------
+
 //port
 const PORT = process.env.PORT || 8080;
 
